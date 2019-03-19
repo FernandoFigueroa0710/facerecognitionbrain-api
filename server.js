8709897af333e46aa4d0017cfd3c3ee96af5cb46
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-19 13:19:21 -0600

    server index set up

--- server.js
+++ server.js
@@ -1,11 +1,54 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
+const knex = require("knex");
 
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "fernandoadan",
+    password: "",
+    database: "smart-brain"
+  }
+});
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("This is working");
+  res.send(db.users);
 });
 
-app.listen(3000, () => {
-  console.log("I am running on port 3000!");
+app.post("/signin", (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
 });
+
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfile(req, res, db);
+});
+
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
+
+app.post("imageurl", (req, res) => {
+  image.handleApiCall(req, res);
+});
+
+app.listen(process.env.PORT || 3001, () => {
+  console.log(`I am running on port ${process.env.PORT}`);
+});
+
+/*
+/ --> res = this is working
+/signin --> POST = sucess/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+ */
